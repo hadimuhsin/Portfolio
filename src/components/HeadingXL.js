8e7e47655663f6c43bde08163e484297be536373
Lewis Llobera <billobera@gmail.com>
2019-07-22 16:46:54 +0200

    edit style

--- src/components/HeadingXL.js
+++ src/components/HeadingXL.js
@@ -9,6 +9,17 @@ export default styled.h1`
   font-weight: 700;
   letter-spacing: -4px;
 
+  background: -webkit-linear-gradient(
+    240deg,
+    hsl(189, 100%, 50%),
+    hsl(174, 79%, 49%),
+    hsl(188, 81%, 59%),
+    hsl(213, 62%, 61%),
+    hsl(240, 100%, 70%)
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
   @media (max-width: 849px) {
     font-size: 45px;
   }
