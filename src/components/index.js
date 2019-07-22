f19128b04620114bf96142d7fad7608b8ab5189c
Lewis Llobera <billobera@gmail.com>
2019-07-22 16:45:58 +0200

    remove unused component

--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,7 @@ import Footer from './Footer';
 import GlobalStyles from './GlobalStyles';
 import Head from './Head';
 import Header from './Header';
-import Heading1 from './Heading1';
-import Heading2 from './Heading2';
+import Heading1 from './HeadingXL';
 import Image from './Image';
 import Layout from './Layout';
 import Nav from './Nav';
@@ -17,7 +16,6 @@ export {
   Head,
   Header,
   Heading1,
-  Heading2,
   Image,
   Layout,
   Nav,
