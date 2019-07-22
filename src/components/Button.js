3a587639143ffd549c45c4e9d4704b4306d9cced
Lewis Llobera <billobera@gmail.com>
2019-07-22 17:14:30 +0200

    edit style and layout

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,34 +1,30 @@
 import styled from 'styled-components';
 
-export default styled.a`
+export default styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 10vh;
-  margin-bottom: 10vh;
+  margin: 10vh auto 5vh auto;
+  padding: 20px 50px;
 
-  width: 25%;
-  font-size: 28px;
-  line-height: 3;
+  width: 300px;
+  font-size: 26px;
   font-weight: bold;
   border-radius: 100px;
-  background-image: linear-gradient(
-    45deg,
-    hsl(330, 100%, 63%),
-    hsl(220, 100%, 54%)
-  );
-  color: var(--background-color);
+  border: 3px solid var(--accent-color);
+  color: var(--accent-color);
   transition: box-shadow 342ms cubic-bezier(0.23, 1, 0.32, 1);
 
   :hover {
-    box-shadow: 0px 9px 0 0 hsla(0, 0%, 17%, 1);
+    box-shadow: 0px 9px 0 0 var(--accent-color);
+  }
+
+  :active {
+    box-shadow: 0px 9px 0 0 var(--accent-color);
   }
 
   @media (max-width: 849px) {
-    width: 75%;
-    font-size: 23px;
+    font-size: 20px;
   }
 `;
