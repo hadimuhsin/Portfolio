7ebb0c764972f7de87f069910d80871173928832
Lewis Llobera <billobera@gmail.com>
2019-07-22 18:05:44 +0200

    edit style

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,5 +2,9 @@ import styled from 'styled-components';
 import Img from 'gatsby-image';
 
 export default styled(Img)`
-  margin: 0 auto 3vh auto;
+  margin: 0 5% 5vh 5%;
+
+@media (max-width: 849px) {
+  margin: 0 0 5vh 0;
+}
 `;
